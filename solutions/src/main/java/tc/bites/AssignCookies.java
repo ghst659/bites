1f839a508ae11f48eff3e342e773f54d1b4af67b
ghst659 <ghst659@github.com>
2017-07-01 17:59:42 -0700

    AssignCookies solution.

---
+++ solutions/src/main/java/tc/bites/AssignCookies.java
@@ -0,0 +1,81 @@
+package tc.bites;
+
+import java.util.Arrays;
+
+/**
+ * Assume you are an awesome parent and want to give
+ * your children some cookies. But, you should give each
+ * child at most one cookie. Each child i has a greed factor gi,
+ * which is the minimum size of a cookie that the child will be
+ * content with; and each cookie j has a size sj.
+ * If sj >= gi, we can assign the cookie j to the child i,
+ * and the child i will be content.
+ * Your goal is to maximize the number of your content
+ * children and output the maximum number.
+ * Note:
+ * You may assume the greed factor is always positive.
+ * You cannot assign more than one cookie to one child.
+ * Example 1:
+ * Input: [1,2,3], [1,1]
+ * Output: 1
+ * Explanation: You have 3 children and 2 cookies.
+ * The greed factors of 3 children are 1, 2, 3.
+ * And even though you have 2 cookies, since their size is both 1,
+ * you could only make the child whose greed factor is 1 content.
+ * You need to output 1.
+ * Example 2:
+ * Input: [1,2], [1,2,3]
+ * Output: 2
+ * Explanation: You have 2 children and 3 cookies.
+ * The greed factors of 2 children are 1, 2.
+ * You have 3 cookies and their sizes are big enough to gratify all of the children,
+ * You need to output 2.
+ */
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        if (s == null || s.length < 1 || g == null || g.length < 1) {
+            return 0;
+        }
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int result = 0;
+        boolean done = false;
+        int cookieLo = 0;
+        int cookieHi = s.length - 1;
+        for (int curChild = 0; curChild < g.length && !done; ++curChild) {
+            int foundCookie = findCookie(g[curChild], s, cookieLo, cookieHi);
+            if (foundCookie < 0) {
+                done = true;
+            } else {
+                cookieLo = foundCookie + 1;
+                result += 1;
+            }
+        }
+        return result;
+    }
+    private int findCookie(int greed, int[] s, int siLo, int siHi) {
+        int found = 0;
+        if (siLo > siHi || greed > s[siHi]) {
+            return -1;
+        } else if (greed <= s[siLo]) {
+            return siLo;
+        } else {
+            while (siHi - siLo > 1) {
+                int mid = (siHi + siLo) / 2;
+                if (greed > s[mid]) {
+                    siLo = mid;
+                } else {
+                    siHi = mid;
+                }
+            }
+            if (s[siLo] < greed) {
+                return siHi;
+            } else {
+                for (found = siLo; found > 0 && s[found-1] >= greed; --found) {
+                    // pass
+                }
+            }
+        }
+        return found;
+    }
+}
---
+++ solutions/src/test/java/tc/bites/AssignCookiesTest.java
@@ -0,0 +1,71 @@
+package tc.bites;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+/**
+ * Assume you are an awesome parent and want to give
+ * your children some cookies. But, you should give each
+ * child at most one cookie. Each child i has a greed factor gi,
+ * which is the minimum size of a cookie that the child will be
+ * content with; and each cookie j has a size sj.
+ * If sj >= gi, we can assign the cookie j to the child i,
+ * and the child i will be content.
+ * Your goal is to maximize the number of your content
+ * children and output the maximum number.
+ * Note:
+ * You may assume the greed factor is always positive.
+ * You cannot assign more than one cookie to one child.
+ * Example 1:
+ * Input: [1,2,3], [1,1]
+ * Output: 1
+ * Explanation: You have 3 children and 2 cookies.
+ * The greed factors of 3 children are 1, 2, 3.
+ * And even though you have 2 cookies, since their size is both 1,
+ * you could only make the child whose greed factor is 1 content.
+ * You need to output 1.
+ * Example 2:
+ * Input: [1,2], [1,2,3]
+ * Output: 2
+ * Explanation: You have 2 children and 3 cookies.
+ * The greed factors of 2 children are 1, 2.
+ * You have 3 cookies and their sizes are big enough to gratify all of the children,
+ * You need to output 2.
+ */
+public class AssignCookiesTest {
+    @Test(dataProvider="cookieGen")
+    public void testAssignCookies(int[] g, int[] s, int expected) {
+        AssignCookies sol = new AssignCookies();
+        int actual = sol.findContentChildren(g, s);
+        Assert.assertEquals(actual, expected, String.format("%s %s %d", aStr(g), aStr(s), expected));
+    }
+    @DataProvider(name="cookieGen")
+    public Object[][] cookieGen() {
+        Object[][] cases = {
+            {new int[]{1,2,3}, new int[]{1,1}, 1},
+            {new int[]{1,2}, new int[]{1,2,3}, 2},
+            {new int[]{1}, new int[]{1}, 1},
+            {new int[]{2}, new int[]{1,1,1,1,1,1,1,1,1,1,1}, 0},
+            {new int[]{3,3,3,3,3}, new int[]{3,3,3,3,3}, 5},
+            {new int[]{3,3,3,3,3}, new int[]{4,4,4,4,4}, 5},
+            {new int[]{3,3,3,3,3}, new int[]{2,2,2,2,2}, 0},
+            {new int[]{3,3,3,3,3}, new int[]{1,3,3,3,4}, 4},
+            {new int[]{3,3,3,3,3}, new int[]{3,3,3}, 3},
+            {new int[]{5,5}, new int[]{2,2,2,2,2,2}, 0},
+            {new int[]{5,10}, new int[]{7}, 1}
+        };
+        return cases;
+    }
+    private String aStr(int[] a) {
+        StringBuilder buf = new StringBuilder("[");
+        for (int i = 0; i < a.length; ++i) {
+            if (i > 0) {
+                buf.append(",");
+            }
+            buf.append(Integer.toString(a[i]));
+        }
+        buf.append("]");
+        return buf.toString();
+    }
+}
