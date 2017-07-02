47010bc08f34d595916754195a022241e82f9d67
ghst659 <ghst659@github.com>
2017-07-02 10:43:19 -0700

    Course Schedule solution.

---
+++ solutions/src/main/java/tc/bites/CourseSchedule.java
@@ -0,0 +1,83 @@
+package tc.bites;
+
+import java.util.*;
+
+/**
+ * There are a total of n courses you have to take, labeled from 0 to n - 1.
+ * Some courses may have prerequisites, for example
+ * to take course 0 you have to first take course 1,
+ * which is expressed as a pair: [0,1]
+ * Given the total number of courses and a list of prerequisite pairs,
+ * is it possible for you to finish all courses?
+ * For example:
+ * 2, [[1,0]]
+ * There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0. So it is possible.
+ * 2, [[1,0],[0,1]]
+ * There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0,
+ * and to take course 0 you should also have finished course 1. So it is impossible.
+ * Note:
+ * The input prerequisites is a graph represented by a list of edges,
+ * not adjacency matrices. Read more about how a graph is represented.
+ * You may assume that there are no duplicate edges in the input prerequisites.
+ */
+public class CourseSchedule {
+    private enum VisitState {
+        WIP,
+        POSSIBLE,
+        IMPOSSIBLE
+    };
+    private Map<Integer,List<Integer>> prereqs = new HashMap<>();
+    private Map<Integer, VisitState> possible = new HashMap<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        studyPrereqs(numCourses, prerequisites);
+        boolean allPossible = true;
+        for (int course = 0; allPossible && course < numCourses; ++course) {
+            allPossible = checkCourse(course);
+        }
+        return allPossible;
+    }
+    private boolean checkCourse(int course) {
+        boolean isPossible = true;
+        if (possible.containsKey(course)) {
+            switch (possible.get(course)) {
+                case WIP:
+                    isPossible = false;
+                    possible.put(course, VisitState.IMPOSSIBLE);
+                    break;
+                case POSSIBLE:
+                    isPossible = true;
+                    break;
+                case IMPOSSIBLE:
+                    isPossible = false;
+                    break;
+                default:
+                    System.err.println("impossible state");
+                    break;
+            }
+        } else {
+            possible.put(course, VisitState.WIP);
+            for (Integer pre: prereqs.get(course)) {
+                if (! checkCourse(pre)) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            possible.put(course, isPossible ? VisitState.POSSIBLE : VisitState.IMPOSSIBLE);
+        }
+        return isPossible;
+    }
+    private void studyPrereqs(int numCourses, int[][] prerequisites) {
+        for (int c = 0; c < numCourses; ++c) {
+            prereqs.put(c, new LinkedList<>());
+        }
+        for (int[] pair: prerequisites) {
+            if (! prereqs.containsKey(pair[0])) {
+                prereqs.put(pair[0], new LinkedList<>());
+            }
+            List<Integer> keyPrereqs = prereqs.get(pair[0]);
+            keyPrereqs.add(pair[1]);
+        }
+    }
+}
---
+++ solutions/src/test/java/tc/bites/CourseScheduleTest.java
@@ -0,0 +1,85 @@
+package tc.bites;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+/**
+ * There are a total of n courses you have to take, labeled from 0 to n - 1.
+ * Some courses may have prerequisites, for example
+ * to take course 0 you have to first take course 1,
+ * which is expressed as a pair: [0,1]
+ * Given the total number of courses and a list of prerequisite pairs,
+ * is it possible for you to finish all courses?
+ * For example:
+ * 2, [[1,0]]
+ * There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0. So it is possible.
+ * 2, [[1,0],[0,1]]
+ * There are a total of 2 courses to take.
+ * To take course 1 you should have finished course 0,
+ * and to take course 0 you should also have finished course 1. So it is impossible.
+ * Note:
+ * The input prerequisites is a graph represented by a list of edges,
+ * not adjacency matrices. Read more about how a graph is represented.
+ * You may assume that there are no duplicate edges in the input prerequisites.
+ */
+public class CourseScheduleTest {
+    @Test(dataProvider="courseGen")
+    public void testCourseSchedule(int numCourses, int[][] prerequisites, boolean expected) {
+        CourseSchedule sut = new CourseSchedule();
+        boolean actual = sut.canFinish(numCourses, prerequisites);
+        String tag = String.format("%d %s", numCourses, aaiStr(prerequisites));
+        Assert.assertEquals(actual, expected, tag);
+    }
+    @DataProvider(name="courseGen")
+    public Object[][] courseGen() {
+        Object[][] cases = {
+            {1, new int[][]{
+                new int[]{1,0}
+            }, true},
+            {2, new int[][]{
+                new int[]{1,0},
+                new int[]{0,1}
+            }, false},
+            {3, new int[][]{
+                new int[]{0,1},
+                new int[]{0,2},
+                new int[]{1,2}
+            }, true},
+            {65536, new int[][]{}, true},
+            {3, new int[][]{
+                new int[]{0,1},
+                new int[]{0,2},
+                new int[]{1,2},
+                new int[]{2,0}
+            }, false}
+        };
+        return cases;
+    }
+    private String aaiStr(int[][] aai) {
+        String r = aStr(aai, (int[] ai)->aiStr(ai));
+        return r;
+    }
+    private String aiStr(int[] ai) {
+        Integer[] aI = Arrays.stream(ai).boxed().toArray(Integer[]::new);
+        String r = aStr(aI, (n)->Integer.toString(n));
+        return r;
+    }
+    private <T> String aStr(T[] a, Function<T,String> toStr) {
+        StringBuffer buf = new StringBuffer("[");
+        if (a != null && a.length > 0) {
+            for (int i = 0; i < a.length; ++i) {
+                if (i > 0) {
+                    buf.append(",");
+                }
+                buf.append(toStr.apply(a[i]));
+            }
+        }
+        buf.append("]");
+        return buf.toString();
+    }
+}
