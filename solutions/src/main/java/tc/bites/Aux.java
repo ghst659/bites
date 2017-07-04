afe44e48d1b2c11eae8172ce60f80d7011984a2a
ghst659 <ghst659@github.com>
2017-07-04 11:56:31 -0700

    Aux cleanup

--- solutions/src/main/java/tc/bites/Aux.java
+++ solutions/src/main/java/tc/bites/Aux.java
@@ -7,17 +7,24 @@ import java.util.function.Function;
  * Auxiliary static routines
  */
 public class Aux {
-    public static String aiStr(int[] ai) {
-        Integer[] aI = Arrays.stream(ai).boxed().toArray(Integer[]::new);
-        String r = axStr(aI, (n)->Integer.toString(n));
-        return r;
+    public static String axStr(int[] ai) {
+        return Arrays.toString(ai);
+    }
+    public static String axStr(boolean[] ab) {
+        return Arrays.toString(ab);
+    }
+    public static String axStr(long[] al) {
+        return Arrays.toString(al);
+    }
+    public static <T> String axStr(T[] a) {
+        return axStr(a, (x)->x.toString());
     }
     public static <T> String axStr(T[] a, Function<T,String> toStr) {
         StringBuffer buf = new StringBuffer("[");
         if (a != null && a.length > 0) {
             for (int i = 0; i < a.length; ++i) {
                 if (i > 0) {
-                    buf.append(",");
+                    buf.append(", ");
                 }
                 buf.append(toStr.apply(a[i]));
             }
